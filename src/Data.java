55f6599d6c70abeccd100b1a1317751219a32dc1
Baris Yilmaz <devsaskral@gmail.com>
2022-12-15 12:05:47 +0300

    Dosyadan Okuma

---
+++ src/Data.java
@@ -0,0 +1,15 @@
+public class Data {
+    public int VarisZamani;
+    public int Oncelik;
+    public int ProcessZamani;
+
+    public int getVarisZamani(){
+        return VarisZamani;
+    }
+    public int getOncelik(){
+        return Oncelik;
+    }
+    public int getProcessZamani(){
+        return  ProcessZamani;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.util.List;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException{
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("giris.txt"))) {
+
+            String line = null;
+
+            String specialCharacter = ",";
+
+            //List<String> data = new ArrayList<>();
+            Data data = new Data();
+
+            while ((line = bufferedReader.readLine()) != null) {
+                line = line.replaceAll(" ","");
+
+                String[] values = line.split(specialCharacter);
+
+                data.VarisZamani = Integer.parseInt(values[0]);
+                data.Oncelik = Integer.parseInt(values[1]);
+                data.ProcessZamani = Integer.parseInt(values[2]);
+
+
+            }
+
+
+        }
     }
 }
